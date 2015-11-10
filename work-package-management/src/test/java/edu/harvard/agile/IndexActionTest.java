6235d5510ab7aba0e1e846f2cf0ee7863274677e
Kamran Zameer <kamranzameer@gmail.com>
2015-11-10 18:06:30 -0500

    trying to test build failure notificaiton

--- work-package-management/src/test/java/edu/harvard/agile/IndexActionTest.java
+++ work-package-management/src/test/java/edu/harvard/agile/IndexActionTest.java
@@ -28,6 +28,6 @@ public class IndexActionTest extends TestCase {
         IndexAction action = new IndexAction();
         String result = action.execute();
         assertEquals(Action.SUCCESS, result);
-        System.out.println("Test sucessfull");
+        Systemm.out.println("Test sucessfull");
     }
 }
